8313eddf41c0c5316b7995668f56e7498afe0a53
Erik Swanson <erik@retailnext.net>
2023-06-28 11:42:12 -0700

    fix: keep generated easyjson files up to date (#643)
    *   Run easyjson generator via go:generate
    
        The CI job will now fail if running go generate makes the tree dirty.
    
    *   Format ci.yaml
    
    *   Run go generate
    
    Signed-off-by: Erik Swanson <erik@retailnext.net>

--- manifests/manifest.go
+++ manifests/manifest.go
@@ -1,4 +1,4 @@
-// Copyright 2019 RetailNext, Inc.
+// Copyright 2023 RetailNext, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:generate go run github.com/mailru/easyjson/easyjson -disallow_unknown_fields $GOFILE
 
 package manifests
 
